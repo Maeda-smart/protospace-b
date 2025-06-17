3689b63312d69329a660b1bda7d8d93efdb5ed57
okiryoma511 <ryoma.oki@smart-group.co.jp>
2025-06-17 15:48:05 +0900

    UserDetail

---
+++ protospace-b/src/main/java/in/tech_camp/protospace_b/controller/UserDetailController.java
@@ -0,0 +1,28 @@
+package in.tech_camp.protospace_b.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import in.tech_camp.protospace_b.entity.UserEntity;
+import in.tech_camp.protospace_b.repository.UserDetailRepository;
+import lombok.AllArgsConstructor;
+
+
+@Controller
+@AllArgsConstructor
+public class UserDetailController {
+
+  private final UserDetailRepository userDetailRepository;
+
+  // ユーザー詳細ページ遷移
+  @GetMapping("/users/{userId}")
+  public String showMyPage(@PathVariable("userId") Integer userId, Model model) {
+    UserEntity users = userDetailRepository.findById(userId);
+    
+    model.addAttribute("user", users);
+    return "users/detail";
+  }
+  
+}
