8b0d82b8621f13a8c93812fa8d4d44fed847e892
MaedaHiroki <hiroki.maeda@smart-group.co.jp>
2025-06-17 15:48:06 +0900

    sign upにviewとform modelの対応付けを追加

--- protospace-b/src/main/java/in/tech_camp/protospace_b/controller/signUpViewController.java
+++ protospace-b/src/main/java/in/tech_camp/protospace_b/controller/signUpViewController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import in.tech_camp.protospace_b.form.UserForm;
 import lombok.AllArgsConstructor;
 
 
@@ -12,6 +13,7 @@ import lombok.AllArgsConstructor;
 public class signUpViewController {
   @GetMapping("/users/sign_up")
   public String getMethodName(Model model) {
+      model.addAttribute("userForm", new UserForm());
       return "users/signUp.html";
   }
   
