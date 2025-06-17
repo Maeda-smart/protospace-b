0796fda8148f20436aaf9ae3bbdec5a9e7ad0591
MaedaHiroki <hiroki.maeda@smart-group.co.jp>
2025-06-17 15:53:45 +0900

    rename signUpViewController to signUpController, add redirect to / when submit sign up

--- protospace-b/src/main/java/in/tech_camp/protospace_b/controller/signUpController.java
+++ protospace-b/src/main/java/in/tech_camp/protospace_b/controller/signUpController.java
@@ -2,19 +2,28 @@ package in.tech_camp.protospace_b.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import in.tech_camp.protospace_b.form.UserForm;
 import lombok.AllArgsConstructor;
 
 
+
 @Controller
 @AllArgsConstructor
-public class signUpViewController {
+public class signUpController {
   @GetMapping("/users/sign_up")
   public String getMethodName(Model model) {
       model.addAttribute("userForm", new UserForm());
       return "users/signUp.html";
   }
+  @PostMapping("/user")
+  public String postMethodName(@ModelAttribute("userForm") UserForm userForm, BindingResult result, Model model) {
+      return "redirect:/";
+  }
+  
   
 }
