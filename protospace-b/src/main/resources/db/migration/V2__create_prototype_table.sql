28a5f92121e548791225743d194b0ee1da6f78d5
okiryoma511 <ryoma.oki@smart-group.co.jp>
2025-06-18 11:09:14 +0900

    prototype_migration

--- protospace-b/src/main/resources/db/migration/V2__create_prototype_table.sql
+++ protospace-b/src/main/resources/db/migration/V2__create_prototype_table.sql
@@ -1,8 +1,10 @@
 CREATE TABLE prototype (
-  id          SERIAL       NOT NULL,
-  name        VARCHAR(128) NOT NULL,
-  catchphrase VARCHAR(128) NOT NULL,
-  concept     VARCHAR(128) NOT NULL,
-  img         VARCHAR(512) NOT NULL,
-  PRIMARY KEY (id)
+  id            SERIAL       NOT NULL,
+  prototypeName VARCHAR(128) NOT NULL,
+  catchCopy     VARCHAR(128) NOT NULL,
+  concept       VARCHAR(128) NOT NULL,
+  img           VARCHAR(512) NOT NULL,
+  user_id       INT          NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
