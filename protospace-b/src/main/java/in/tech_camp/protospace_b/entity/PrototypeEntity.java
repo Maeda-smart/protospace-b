b28689cd96baa96de5934449436ddbf92e034d02
okiryoma511 <ryoma.oki@smart-group.co.jp>
2025-06-18 11:22:18 +0900

    仮

---
+++ protospace-b/src/main/java/in/tech_camp/protospace_b/entity/PrototypeEntity.java
@@ -0,0 +1,13 @@
+package in.tech_camp.protospace_b.entity;
+
+import lombok.Data;
+
+@Data
+public class PrototypeEntity {
+  private Integer id;
+  private String prototypeName;
+  private String catchCopy;
+  private String concept;
+  private String img;
+  private 
+}
