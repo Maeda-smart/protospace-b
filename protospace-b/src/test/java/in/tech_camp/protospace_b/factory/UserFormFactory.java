228f334b3c3b38e4df238304e217226750f9cdf9
MaedaHiroki <hiroki.maeda@smart-group.co.jp>
2025-06-18 15:02:33 +0900

    change user form factory values to random strings with faker

--- protospace-b/src/test/java/in/tech_camp/protospace_b/factory/UserFormFactory.java
+++ protospace-b/src/test/java/in/tech_camp/protospace_b/factory/UserFormFactory.java
@@ -1,6 +1,9 @@
 package in.tech_camp.protospace_b.factory;
 
+import java.util.Random;
+
 import com.github.javafaker.Faker;
+
 import in.tech_camp.protospace_b.form.UserForm;
 
 public class UserFormFactory{
@@ -9,14 +12,31 @@ public class UserFormFactory{
   public static UserForm createUser(){
     UserForm userForm = new UserForm();
 
-    userForm.setNickname("Nick");
-    userForm.setEmail("test@example.com");
-    userForm.setPassword("Password");
-    userForm.setPasswordConfirmation("Password");
-    userForm.setProfile("Profile");
-    userForm.setAffiliation("Affiliation");
-    userForm.setPosition("Position");
+    userForm.setNickname(faker.internet().emailAddress());
+    userForm.setEmail(faker.name().username());
+    userForm.setPassword(faker.internet().password(6, 128));
+    userForm.setPasswordConfirmation(userForm.getPassword());
+    userForm.setProfile(randomTextInRange(1, 128));
+    userForm.setAffiliation(randomTextInRange(1, 128));
+    userForm.setPosition(randomTextInRange(1, 128));
 
     return userForm;
+  }
+
+  private static String randomTextInRange(int min, int max){
+    Faker faker = new Faker();
+    Random rand = new Random();
+    StringBuilder sb = new StringBuilder();
+    while (sb.length() < min) {
+      sb.append(faker.lorem().sentence());
+      sb.append("");
+    }
+    int len = min + rand.nextInt(max - min + 1);
+    if (sb.length() > max){
+      sb.setLength(max);
+    } else{
+      sb.setLength(len);
+    }
+    return sb.toString();
   }
 }
