19ed4bfe2f2c52c841bf7055c78996c8935a6b31
rikuhamada <riku.hamada@smart-group.co.jp>
2025-06-18 17:14:03 +0900

    プロトタイプ投稿機能の追加

---
+++ protospace-b/src/main/java/in/tech_camp/protospace_b/repository/PrototypeNewRepository.java
@@ -0,0 +1,15 @@
+package in.tech_camp.protospace_b.repository;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import in.tech_camp.protospace_b.entity.PrototypeEntity;
+
+@Mapper
+public interface PrototypeNewRepository {
+
+    @Insert("INSERT INTO prototype (prototypeName, catchCopy, concept, img, user_id) " +
+            "VALUES (#{prototypeName}, #{catchCopy}, #{concept}, #{img}, #{user.id})")
+    @Options(useGeneratedKeys = true, keyProperty = "id")
+    void insert(PrototypeEntity prototype);
+}
