2a0629b497bd00f107e7f30666de337ffe8c1e3c
MaedaHiroki <hiroki.maeda@smart-group.co.jp>
2025-06-20 15:05:01 +0900

    add prototype created by at top page

--- protospace-b/src/main/java/in/tech_camp/protospace_b/repository/PrototypeShowRepository.java
+++ protospace-b/src/main/java/in/tech_camp/protospace_b/repository/PrototypeShowRepository.java
@@ -2,7 +2,10 @@ package in.tech_camp.protospace_b.repository;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Many;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 
 import in.tech_camp.protospace_b.entity.PrototypeEntity;
@@ -10,6 +13,10 @@ import in.tech_camp.protospace_b.entity.PrototypeEntity;
 @Mapper
 public interface PrototypeShowRepository {
   @Select("SELECT * FROM prototype")
+  @Results(value = {
+      @Result(property = "id", column = "id"),
+      @Result(property = "user", column="user_id", many=@Many(select="in.tech_camp.protospace_b.repository.UserDetailRepository.findById"))
+  })
   List<PrototypeEntity> showAll();
 
   @Select("SELECT * FROM prototype WHERE user_id = #{userId}")
