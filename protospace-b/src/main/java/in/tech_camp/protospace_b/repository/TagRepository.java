34b851ff82768da0baa853ceecfc1c71946c0e6b
MaedaHiroki <hiroki.maeda@smart-group.co.jp>
2025-06-24 15:57:22 +0900

    add tag repository

---
+++ protospace-b/src/main/java/in/tech_camp/protospace_b/repository/TagRepository.java
@@ -0,0 +1,33 @@
+package in.tech_camp.protospace_b.repository;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.One;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
+
+import in.tech_camp.protospace_b.entity.PrototypeEntity;
+import in.tech_camp.protospace_b.entity.TagEntity;
+
+@Mapper
+public interface TagRepository {
+  @Insert("INSERT INTO tags(tag_name) VALUES (#{tagName})")
+  @Options(useGeneratedKeys = true, keyProperty = "id")
+  void insert(String tagName);
+
+  @Select("SELECT * FROM tags WHERE tag_name = #{tagName}")
+  TagEntity justSameTag(String tagName);
+
+  @Select("SELECT tags.id, tags.tag_name FROM tags INNER JOIN prototype_tags ON tags.id = prototype_tags.tags_id WHERE prototype_tags.prototype_id = #{prototypeId}")
+  List<TagEntity> prototypeTags(Integer prototypeId);
+
+  @Select("SELECT p.id, p.prototypeName, p.catchCopy, p.concept, p.imgPath FROM prototype p INNER JOIN prototype_tags ON prototype.id = prototype_tags.prototype_id INNER JOIN tags ON tags.id = prototype_tags.tags_id WHERE prototype_tags.tags_id = #{tagId}")
+  @Results(value={
+    @Result(property="user", column="prototype.user_id", one=@One(select = "in.tech_camp.protospace_b.repository.UserNewRepository.findById"))
+  })
+  List<PrototypeEntity> tagPrototypes(Integer tagId);
+}
