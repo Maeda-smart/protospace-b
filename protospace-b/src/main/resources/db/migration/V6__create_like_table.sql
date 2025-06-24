fe0ee4a5481f42dd279abb7452dcdd9608b5c0ea
okiryoma511 <ryoma.oki@smart-group.co.jp>
2025-06-24 11:51:22 +0900

    Create V6__create_like_table.sql

---
+++ protospace-b/src/main/resources/db/migration/V6__create_like_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE like (
+    prototype_id INT NOT NULL,
+    user_id INT NOT NULL,
+
+    PRIMARY KEY (prototype_id, user_id),
+    FOREIGN KEY (prototype_id) REFERENCES prototype(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+
+);
