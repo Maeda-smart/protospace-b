db130191d4e6dc7d62a7ff8a7d52d1d7c191c478
okiryoma511 <ryoma.oki@smart-group.co.jp>
2025-06-24 15:07:45 +0900

    基本機能の実装

--- protospace-b/src/main/java/in/tech_camp/protospace_b/controller/UserDetailController.java
+++ protospace-b/src/main/java/in/tech_camp/protospace_b/controller/UserDetailController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import in.tech_camp.protospace_b.entity.PrototypeEntity;
 import in.tech_camp.protospace_b.entity.UserEntity;
+import in.tech_camp.protospace_b.repository.NiceRepository;
 import in.tech_camp.protospace_b.repository.PrototypeShowRepository;
 import in.tech_camp.protospace_b.repository.UserDetailRepository;
 import lombok.AllArgsConstructor;
@@ -20,15 +21,21 @@ public class UserDetailController {
 
   private final UserDetailRepository userDetailRepository;
   private final PrototypeShowRepository prototypeShowRepository;
+  private final NiceRepository niceRepository;
 
   // ユーザー詳細ページ遷移
   @GetMapping("/users/{userId}")
   public String showMyPage(@PathVariable("userId") Integer userId, Model model) {
-    UserEntity users = userDetailRepository.findById(userId);
     
+    UserEntity users = userDetailRepository.findById(userId);
     model.addAttribute("user", users);
+    // ログインユーザーの投稿取得
     List<PrototypeEntity> prototypes = prototypeShowRepository.showByUserId(users.getId());
     model.addAttribute("prototypes", prototypes);
+
+    // ログインユーザーのいいねした投稿取得
+    List<PrototypeEntity> nicePrototypes = niceRepository.findNiceByUserId(userId);
+    model.addAttribute("nicePrototypes", nicePrototypes);
     return "users/detail";
   }
   
