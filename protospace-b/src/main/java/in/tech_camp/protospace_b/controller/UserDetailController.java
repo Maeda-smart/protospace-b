162b2d2b4c3748ef73f481c7ed0961e6bb91f6b1
okiryoma511 <ryoma.oki@smart-group.co.jp>
2025-06-24 15:34:41 +0900

    csrf

--- protospace-b/src/main/java/in/tech_camp/protospace_b/controller/UserDetailController.java
+++ protospace-b/src/main/java/in/tech_camp/protospace_b/controller/UserDetailController.java
@@ -2,11 +2,13 @@ package in.tech_camp.protospace_b.controller;
 
 import java.util.List;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import in.tech_camp.protospace_b.custom_user.CustomUserDetail;
 import in.tech_camp.protospace_b.entity.PrototypeEntity;
 import in.tech_camp.protospace_b.entity.UserEntity;
 import in.tech_camp.protospace_b.repository.NiceRepository;
@@ -25,16 +27,19 @@ public class UserDetailController {
 
   // ユーザー詳細ページ遷移
   @GetMapping("/users/{userId}")
-  public String showMyPage(@PathVariable("userId") Integer userId, Model model) {
-    
+  public String showMyPage(@PathVariable("userId") Integer userId, @AuthenticationPrincipal CustomUserDetail currentUser, Model model) {
+
     UserEntity users = userDetailRepository.findById(userId);
     model.addAttribute("user", users);
+
+    Integer loginUser = currentUser.getId();
+
     // ログインユーザーの投稿取得
     List<PrototypeEntity> prototypes = prototypeShowRepository.showByUserId(users.getId());
     model.addAttribute("prototypes", prototypes);
 
     // ログインユーザーのいいねした投稿取得
-    List<PrototypeEntity> nicePrototypes = niceRepository.findNiceByUserId(userId);
+    List<PrototypeEntity> nicePrototypes = niceRepository.findNiceByUserId(loginUser);
     model.addAttribute("nicePrototypes", nicePrototypes);
     return "users/detail";
   }
