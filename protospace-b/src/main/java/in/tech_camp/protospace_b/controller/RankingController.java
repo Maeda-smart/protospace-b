6ed55f50e640605be9e60bf94cdc8767dfdaceb9
okiryoma511 <ryoma.oki@smart-group.co.jp>
2025-06-26 15:17:56 +0900

    最低限のビュー完成

--- protospace-b/src/main/java/in/tech_camp/protospace_b/controller/RankingController.java
+++ protospace-b/src/main/java/in/tech_camp/protospace_b/controller/RankingController.java
@@ -3,6 +3,7 @@ package in.tech_camp.protospace_b.controller;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
@@ -11,7 +12,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import in.tech_camp.protospace_b.custom_user.CustomUserDetail;
 import in.tech_camp.protospace_b.entity.PrototypeEntity;
+import in.tech_camp.protospace_b.entity.ReadStatusEntity;
+import in.tech_camp.protospace_b.entity.UserEntity;
 import in.tech_camp.protospace_b.repository.NiceRepository;
+import in.tech_camp.protospace_b.repository.UserDetailRepository;
+import in.tech_camp.protospace_b.service.ReadStatusService;
 import lombok.AllArgsConstructor;
 
 @Controller
@@ -19,10 +24,27 @@ import lombok.AllArgsConstructor;
 public class RankingController {
 
   private final NiceRepository niceRepository;
+  private final UserDetailRepository userDetailRepository;
+  private final ReadStatusService readStatusService;
   
   // ランキングページに遷移
   @GetMapping("/prototypes/ranking")
   public String showRanking(@AuthenticationPrincipal CustomUserDetail currentUser, Model model) {
+
+    // 既読・未読を管理
+    if (currentUser != null) {
+      Integer userId = currentUser.getId();
+      UserEntity user = userDetailRepository.findById(userId);
+      model.addAttribute("user", user);
+
+      List<ReadStatusEntity> readList = readStatusService.findAllByUserId(userId);
+      Map<Integer, Boolean> readStatusMap = readList.stream()
+        .collect(Collectors.toMap(
+          ReadStatusEntity::getPrototypeId,
+          r -> true
+        ));
+      model.addAttribute("readStatusMap", readStatusMap);
+    }
 
     // いいね数順に並び替えたプロトタイプを取得
     List<PrototypeEntity> rankingPrototypes = niceRepository.findPrototypesOrderByCountDesc();
