11901670ff6a2bfee114d6618425990753f95157
039nh <naho.nakamura@smart-group.co.jp>
2025-07-01 11:02:25 +0900

    notification既読

--- protospace-b/src/main/java/in/tech_camp/protospace_b/service/NotificationService.java
+++ protospace-b/src/main/java/in/tech_camp/protospace_b/service/NotificationService.java
@@ -1,5 +1,5 @@
 package in.tech_camp.protospace_b.service;
-
+ 
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -7,13 +7,24 @@ import org.springframework.stereotype.Service;
 import in.tech_camp.protospace_b.entity.CommentNotificationEntity;
 import in.tech_camp.protospace_b.repository.CommentNotificationRepository;
 import lombok.RequiredArgsConstructor;
-
+ 
 @Service
 @RequiredArgsConstructor
 public class NotificationService {
-  private final CommentNotificationRepository commentNotificationRepository;
-
-  public List<CommentNotificationEntity> getCommentNotifications(Integer userId) {
-    return commentNotificationRepository.findCommentNotificationByUserId(userId);
+ 
+  private final CommentNotificationRepository notificationRepository;
+ 
+  public List<CommentNotificationEntity> getUnreadNotifications(Integer userId) {
+    return notificationRepository.findUnreadByRecipientId(userId);
+  }
+ 
+  public void markAsRead(Integer notificationId) {
+    System.out.println("markAsRead called with notificationId = " + notificationId);
+    notificationRepository.markAsRead(notificationId);
+    System.out.println("update executed for notificationId = " + notificationId);
+  }
+ 
+  public void createNotification(CommentNotificationEntity notification) {
+    notificationRepository.insertNotification(notification);
   }
 }
